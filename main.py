c23b4c9f7765dc0532d47656bd5765b18395a638
Richard Bajan <riso.bajan@gmail.com>
2025-02-10 15:50:13 +0100

    Repair body/subject

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import requests
 from send_email import send_email
 
-topic = "tesla"
+category = "business"
 
 api_key = "3762647bc9f2433c94b8e902dbcbfc74"
-url = ("https://newsapi.org/v2/everything?"
-       f"q={topic}&"
-       "from=2025-01-09&"
-       "sortBy=publishedAt&"
-       "apiKey=3762647bc9f2433c94b8e902dbcbfc74&language=en")
+url = ("https://newsapi.org/v2/top-headlines?"
+       "country=us&"
+       f"category={category}&"
+       "apiKey=3762647bc9f2433c94b8e902dbcbfc74")
 
 # Make request
 request = requests.get(url)
@@ -19,11 +18,11 @@ content = request.json()
 # Access the article titles and description
 body = ""
 for article in content["articles"][:20]:
-    if article["title"] is not None:
-        body = "Subject: Today's new" \
-                + "\n" + body + article["title"] + "\n" \
-                + article["description"] + "\n" \
-                + article["url"] + 2*"\n"
+    if article["content"] is not None:
+        body =  (body + article["title"] + "\n"
+                + article["description"] + "\n"
+                + article["url"] + 2*"\n")
 
+body = "Subject: News" + "\n" + body
 body = body.encode("utf-8")
 send_email(message=body)
